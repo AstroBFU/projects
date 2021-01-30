d8fdcaa3aa7991e17662f66c14750012aea2c8a1
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2021-01-30 14:20:51 +0200

    Update osc.py
    repair code

--- osc.py
+++ osc.py
@@ -2,25 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
+low_l = 0
+low_h = 10
+
+frames = 1000
+
 fig, ax = plt.subplots()
 anim_object, = plt.plot([], [], '-', lw=2)
 plt.axis('equal')
-ax.set_xlim(-2*np.pi, 2*np.pi)
-ax.set_ylim(-2*np.pi, 2*np.pi)
+ax.set_xlim(low_l, low_h)
+ax.set_ylim(low_l-10, low_h)
 
 def garmonic_f(t, A = 1, B = 1, w = np.pi, k = np.pi):
-	x = t
-	y = A*np.cos(w*t - k*x) + B*np.cos(w*t + k*x)
-	return x, y
+  x = np.linspace(0, 2*np.pi, frames)
+  y = A*np.cos(w*t - k*x) + B*np.cos(w*t + k*x)
+  return x, y
 
-'''def oscillation(x, t):	
-	phi = np.arange(0, 2*np.pi, 0.01)
-	y = r*np.sin(phi)
-	return x, y
-'''
 
 def update(frame):
-	anim_object.set_data(garmonic_f(t = frame))
+	anim_object.set_data(garmonic_f(A = 0.5, t = frame))
 
-anim = FuncAnimation(fig, update, frames = 500, interval=30)
-anim.save('oscillation.gif')
+anim = FuncAnimation(fig, update, frames, interval=300)
+plt.show()
